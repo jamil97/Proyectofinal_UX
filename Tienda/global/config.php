c4a16d7d5097f34313e43485cb890a64be20b897
Jamil Villeda <jamvilleda97@live.com>
2020-03-26 23:50:38 -0600

    Progreso tab de pago

--- Tienda/global/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("KEY","develoteca"); //Llave de encriptacion
-define("COD","AES-128-ECB"); //Metodo de encriptacion
-define("SERVIDOR","localhost");
-define("USUARIO","root");
-define("PASSWORD","");
-define("BD","tienda");
-?>
--- Tienda/templates/pie.php
+++
@@ -1,4 +0,0 @@
-</div>
-<<div class="col-12 text-center">UX </div>
-</body>
-</html>
