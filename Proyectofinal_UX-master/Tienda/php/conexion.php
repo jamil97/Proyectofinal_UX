c3e42a189666c7ccaee18b99765beb6589fb65cd
WilmerFloresMolina <wilmerflores-tux2@unitec.edu>
2020-03-27 16:18:25 -0600

    Delete conexion.php

--- Proyectofinal_UX-master/Tienda/php/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host="brmncxwybnll9uagy5np-mysql.services.clever-cloud.com";
-$user="uwuwyf5pentlcbgp";
-$password="CWZHGE7Ji8FGL7wYwjyU";
-$db="brmncxwybnll9uagy5np";
-$con = new mysqli($host,$user,$password,$db);
-
-?>
