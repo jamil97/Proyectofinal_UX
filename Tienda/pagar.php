18519e383eaea11074e15d9b16f9418248a0e6bc
Jamil Villeda <jamvilleda97@live.com>
2020-03-27 00:33:16 -0600

    Funcionamiento de Paypal

--- Tienda/pagar.php
+++ Tienda/pagar.php
@@ -45,8 +45,41 @@ include 'templates/cabecera.php';
     <hr class="my-4">
     <p class="lead"> Estas a punto de pagar con Paypal la cantidad de: 
         <h4>$<?php echo number_format($total,2);?></h4>
+          <!-- Set up a container element for the button PAYPAL -->
+        <div id="paypal-button-container"></div>
     </p>
     <p>Los productos se procesaran una vez que se procese el pago. </p>
 </div>
+
+<!-- PAGO CON PAYPAL (AUTOGENERADO DE LA PAGINA OFICIAL) -->
+<!DOCTYPE html>
+
+<head>
+    <!-- Add meta tags for mobile and IE -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+</head>
+
+<body>
+  
+
+    <!-- Include the PayPal JavaScript SDK -->
+    <script src="https://www.paypal.com/sdk/js?client-id=sb&currency=USD"></script>
+
+    <script>
+        // Render the PayPal button into #paypal-button-container
+        paypal.Buttons({
+
+            style: {
+                color:  'blue',
+                shape:  'pill',
+                label:  'pay',
+                height: 40
+            }
+
+        }).render('#paypal-button-container');
+    </script>
+</body>
+<!-- PAGO CON PAYPAL (AUTOGENERADO DE LA PAGINA OFICIAL) -->
 
 <?php include 'templates/pie.php';?>
